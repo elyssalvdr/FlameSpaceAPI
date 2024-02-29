d7205a11ef97b2f469b11eef8e4c7308e8604520
Elyssa Salvador <elta.salvador.up@phinmaed.com>
2024-02-29 11:03:03 +0800

    flamespace api

---
+++ routes/api.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\DashboardController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\RoomController;
+use App\Http\Controllers\API\ScheduleController;
+use App\Http\Controllers\API\CalendarController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Dashboard
+Route::get('/dashboard', [DashboardController::class, 'index']);
+
+// Users
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/users/search', [UserController::class, 'search']);
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+// Rooms
+Route::get('/rooms', [RoomController::class, 'index']);
+Route::get('/rooms/{id}', [RoomController::class, 'show']);
+Route::get('/rooms/filter/{building}', [RoomController::class, 'filterByBuilding']);
+Route::post('/rooms', [RoomController::class, 'store']);
+Route::put('/rooms/{id}', [RoomController::class, 'update']);
+Route::delete('/rooms/{id}', [RoomController::class, 'destroy']);
+
+// Schedules
+Route::get('/schedules', [ScheduleController::class, 'index']);
+Route::get('/schedules/{id}', [ScheduleController::class, 'show']);
+Route::get('/schedules/filter/{room}', [RoomController::class, 'filterByRoom']);
+Route::post('/schedules', [ScheduleController::class, 'store']);
+Route::put('/schedules/{id}', [ScheduleController::class, 'update']);
+Route::delete('/schedules/{id}', [ScheduleController::class, 'destroy']);
+
+// Calendar
+Route::get('calendar', [CalendarController::class, 'index']);
+Route::get('/rooms/filter', [RoomController::class, 'filter']);
